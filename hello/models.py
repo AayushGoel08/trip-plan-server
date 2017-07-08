099a75822cade9f0762adf75a56896f15799048b
Aayush Goel <Aayush Goel>
2017-07-09 00:06:02 +0530

    User Trips Database

--- hello/models.py
+++ hello/models.py
@@ -7,6 +7,6 @@ class Greeting(models.Model):
 class Trips(models.Model):
     fbid = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
-    start = models.CharField(max_length=100)
-    end = models.CharField(max_length=100)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
     status = models.IntegerField()
