22b10ebf6167ca5f465ecb6b986136970621d266
Aayush Goel <Aayush Goel>
2017-07-08 23:44:22 +0530

    Userdb

--- hello/views.py
+++ hello/views.py
@@ -123,7 +123,7 @@ def ilp():
         return "Solution not found"
 
 def insertRecord(postData):
-    userTrip = Trips(1,postData["fbid"],postData["city"],postData["start"],postData["end"],1)
+    userTrip = Trips(None,postData["fbid"],postData["city"],postData["start"],postData["end"],1)
     #userTrip = UserTrips(postData["fbid"],postData["city"],"2017-06-30 16:00","2017-06-30 09:00",1)
     userTrip.save()
 
