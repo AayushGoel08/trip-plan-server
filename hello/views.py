1ea7ba526e905a5589be405a274308a31649262b
Aayush Goel <Aayush Goel>
2017-07-13 12:26:03 +0530

    New ILP

--- hello/views.py
+++ hello/views.py
@@ -30,8 +30,8 @@ def index(request):
             for loc in locs:
                 timeplaces.append(dateconversion(start,end,loc.acttype,loc.hours))
                 staytimeplaces.append(loc.time)
-            #response = ilp(sleepstart, postData["home0"], postData["places"],timeplaces,staytimeplaces, numduration[0], numduration[1])
-            return JsonResponse({"data": timeplaces, "found": staytimeplaces})
+            response = ilp(sleepstart, postData["home0"], postData["places"],timeplaces,staytimeplaces, numduration[0], numduration[1])
+            return JsonResponse({"data": response[0], "found": response[1]})
         elif(postData["type"]=="Count"):
             return JsonResponse({"data": Trips.objects.count()})
         elif(postData["type"]=="Insert"):
