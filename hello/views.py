d864e62af6c0f35d9040b24f575e5b52e382db5f
Aayush Goel <Aayush Goel>
2017-07-13 18:00:36 +0530

    Linking Trips DB

--- hello/views.py
+++ hello/views.py
@@ -12,10 +12,25 @@ import json
 import datetime
 from ilp import *
 
-# Create your views here.
+
+def insertTripRecord(postData):
+    tripid = Trips.objects.filter(fbid = postData["fbid"],city = postData["fbid"]).count()+1
+    start = datetime.datetime(postData["start"])
+    end = datetime.datetime(postData["end"])
+    possibles = ""
+    for loc in Locations.objects.filter(city = postData["city"]):
+        timepoint = dateconversion(start,end,loc.acttype,loc.hours)
+        if(timepoint!=[]):
+            possibles = possibles+str(loc.locid)+","
+    possibles = possibles[:-1]
+    userTrip = Trips(None,postData["fbid"],tripid,postData["city"],postData["start"],postData["end"],1,possibles,"")
+    userTrip.save()
+    
+def insertLocationRecord(postData):
+    loc = Locations(None,postData["city"],postData["locid"],postData["activity"],postData["address"],postData["price"],postData["acttype"],postData["hours"],postData["time"],postData["book"])
+    loc.save()
+    
 def index(request):
-    #string = ilp()
-    #return JsonResponse({"data": string})
     if(request.method=='POST'):
         jsonData = request.body.decode("utf-8")
         postData = json.loads(jsonData)
@@ -34,17 +49,21 @@ def index(request):
                 locids.append(loc.locid)
             response = ilp(sleepstart, postData["home0"], locids,timeplaces,staytimeplaces, numduration[0], numduration[1])
             return JsonResponse({"data": response[0], "found": response[1], "ids": locids, "stays": staytimeplaces, "travel": response[2]})
+
         elif(postData["type"]=="Count"):
             return JsonResponse({"data": Trips.objects.count()})
+
         elif(postData["type"]=="Insert"):
             insertRecord(postData)
             return JsonResponse({"data": Trips.objects.count()})
+
         elif(postData["type"]=="DeleteAll"):
             Trips.objects.all().delete()
             return JsonResponse({"data": Trips.objects.count()})
+
         elif(postData["type"]=="GetData"):
             usertrips = Trips.objects.filter(fbid = postData["fbid"])
-            userentries = {"records": [[entry.city,entry.start,entry.end] for entry in usertrips]}
+            userentries = {"records": [[entry.city,entry.start,entry.end, entry.possibles] for entry in usertrips]}
             return JsonResponse({"data": userentries})
         
 def db(request):
@@ -53,9 +72,11 @@ def db(request):
         postData = json.loads(jsonData)
         if(postData["type"]=="Count"):
             return JsonResponse({"data": Locations.objects.count()})
+
         elif(postData["type"]=="Insert"):
             insertLocationRecord(postData)
             return JsonResponse({"data": Locations.objects.count()})
+
         elif(postData["type"]=="DeleteAll"):
             Locations.objects.all().delete()
             return JsonResponse({"data": Locations.objects.count()})
