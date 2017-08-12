7c771fed3ca7a6cbcd7bae74192b307c2df6762d
Aayush Goel <Aayush Goel>
2017-08-12 23:40:18 +0530

    Migration to LocStore

--- hello/views.py
+++ hello/views.py
@@ -532,6 +532,11 @@ def entries(request):
                 userentries = {"records": [[loc.city,loc.locid,loc.name,loc.title,loc.hashtag,loc.description,loc.imagelink,loc.time,loc.rating,loc.price,loc.book,loc.deposit,loc.acttype,loc.hours,loc.provider,loc.website,loc.address,loc.coordinates] for loc in locs]}
                 return JsonResponse({"data": userentries})
 
+            elif(postData["type"]=="GetLocData"):
+                locs = LocStore.objects.filter(city = postData["city"],locid = postData["locid"])
+                userentries = {"records": [[loc.city,loc.locid,loc.name,loc.title,loc.hashtag,loc.description,loc.imagelink,loc.time,loc.rating,loc.price,loc.book,loc.deposit,loc.acttype,loc.hours,loc.provider,loc.website,loc.address,loc.coordinates] for loc in locs]}
+                return JsonResponse({"data": userentries})
+
             elif(postData["type"]=="GetAllCityData"):
                 locs = LocStore.objects.filter(city = postData["city"])
                 userentries = {"records": [[loc.city,loc.locid,loc.name,loc.title,loc.hashtag,loc.description,loc.imagelink,loc.time,loc.rating,loc.price,loc.book,loc.deposit,loc.acttype,loc.hours,loc.provider,loc.website,loc.address,loc.coordinates] for loc in locs]}
