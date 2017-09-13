2bb245f56d6edaf44df6d7c1163a424f2c80badb
Aayush Goel <Aayush Goel>
2017-09-13 11:54:19 +0530

    Trial Worker

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'i+acxn5(akgsn!sr4^qgf(^m&*@+g1@u^t@=8s@axc41ml*f=s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+DJANGO_SETTINGS_MODULE=settings rq worker high default low
 
 # Application definition
 
