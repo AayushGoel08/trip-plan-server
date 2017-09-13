3fd01450d68c1ac13c6f934809c30f2f1ed98671
Aayush Goel <Aayush Goel>
2017-09-13 12:00:14 +0530

    Trial

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -17,7 +17,7 @@ import rq
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
+DJANGO_SETTINGS_MODULE=settings
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -27,7 +27,6 @@ SECRET_KEY = 'i+acxn5(akgsn!sr4^qgf(^m&*@+g1@u^t@=8s@axc41ml*f=s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-DJANGO_SETTINGS_MODULE=settings rq worker high default low
 
 # Application definition
 
