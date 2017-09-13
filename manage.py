2e2b8c0b64f825f1782c9130938471c67f5dbed5
Aayush Goel <Aayush Goel>
2017-09-13 12:25:32 +0530

    Trial

--- manage.py
+++ manage.py
@@ -5,8 +5,7 @@ from django.core.wsgi import get_wsgi_application
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
-    os.environ["DJANGO_SETTINGS_MODULE"] = "gettingstarted.settings"
-    application = get_wsgi_application()
+    
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
--- worker.py
+++ worker.py
@@ -4,7 +4,7 @@ import redis
 from rq import Worker, Queue, Connection
 import django
 from django.conf import settings
-
+from django.core.wsgi import get_wsgi_application
 listen = ['high', 'default', 'low']
 
 redis_url = os.getenv('REDISTOGO_URL', 'redis://redistogo:f5acd0aff98e5377361eb9136497a053@tarpon.redistogo.com:10145')
@@ -12,6 +12,8 @@ redis_url = os.getenv('REDISTOGO_URL', 'redis://redistogo:f5acd0aff98e5377361eb9
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
+    os.environ["DJANGO_SETTINGS_MODULE"] = "gettingstarted.settings"
+    application = get_wsgi_application()
     settings.configure()
     with Connection(conn):
         worker = Worker(map(Queue, listen))
