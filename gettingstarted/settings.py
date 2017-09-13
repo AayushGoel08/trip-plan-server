12fa69c951dab852ab80c2c2cde04df57f8f7672
Aayush Goel <Aayush Goel>
2017-09-13 12:01:25 +0530

    Trial

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -17,7 +17,7 @@ import rq
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-DJANGO_SETTINGS_MODULE=settings
+DJANGO_SETTINGS_MODULE=trip-plan-router.settings
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
